5e05978b36efc0cad933d082f1afbfdc7e0ffb5b
Gustavo Marinho <101999608+GustavoMarinho82@users.noreply.github.com>
2024-12-15 21:51:19 -0300

    commit

---
+++ C/maquinaEstado.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+const char SEPARACOES[7] = " .,;:\n\t";
+
+char eh_separacao(char caracter);
+
+int main() {
+	char estado = 0;
+	// 0 -> Out of word
+	// 1 -> In word
+
+	char frase[1024];
+	fgets(frase, 1024, stdin); //Da errado se usar o scanf, porque ele encerra a leitura da string quando tem um " "
+    	
+	int contador = 0;
+	int p = 0;
+
+	while (frase[p] !=  '\0') {
+		switch (estado) {
+			case 0:
+				if (!eh_separacao(frase[p])) {
+					contador++;
+					estado = 1;
+				}
+			break;
+			
+			case 1:
+				if (eh_separacao(frase[p]))
+					estado = 0;
+			break;
+		}
+		
+		p++;
+	}
+	
+	printf("%d \n", contador);
+	
+	return 0;
+}
+
+char eh_separacao(char caracter) {
+	for (int i = 0; i < 7; i++){
+		if (caracter == SEPARACOES[i])
+			return 1;
+	}
+	
+	return 0;
+}
