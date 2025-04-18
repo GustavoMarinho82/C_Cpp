9af288e94baebe8ba1bcca7d2a2f773990452a3b
GustavoMarinho82 <gustavo.mariinho06@gmail.com>
2025-04-18 16:16:19 -0300

    commit

--- C/torreHanoi/Ex01.c
+++ C/torreHanoi/Ex01.c
@@ -11,6 +11,34 @@
     #define ESPERAR(s) sleep((s))
 #endif
 
+void imprimir_torres(int, int, ...);
+void resolver_torre_hanoi(int, Stack*, Stack*, Stack*);
+void mover_discos(int, Stack*, Stack*, Stack*, int, Stack*, Stack*, Stack*);
+
+int main() {
+	int qtd_discos;
+	
+	printf("TORRE DE HANÓI \nQuantos discos na primeira torre: ");
+	scanf("%d", &qtd_discos);
+	
+	Stack* origem = new_stack(qtd_discos);
+	Stack* auxiliar = new_stack(qtd_discos);
+	Stack* destino = new_stack(qtd_discos);
+	
+	for (int i = qtd_discos; i > 0; i--) 
+		push(origem, i);
+	
+	imprimir_torres(qtd_discos, 3, origem, auxiliar, destino);
+	
+	resolver_torre_hanoi(qtd_discos, origem, auxiliar, destino);
+	
+	destroy_stack(origem);
+	destroy_stack(auxiliar);
+	destroy_stack(destino);
+	
+	return 0;
+}
+
 void imprimir_torres(int max_altura, int num_args, ...) {
 	Stack* torres[num_args];
 	int max_largura = max_altura*2 - 1;
@@ -50,6 +78,11 @@ void imprimir_torres(int max_altura, int num_args, ...) {
 	ESPERAR(1);
 }
 
+void resolver_torre_hanoi(int altura, Stack* origem, Stack* auxiliar, Stack* destino) { 
+	// Leva (2^qtd_discos - 1) passos para resolver
+	mover_discos(altura, origem, auxiliar, destino, altura, origem, auxiliar, destino);
+}
+
 void mover_discos(int qtd_discos, Stack* origem, Stack* auxiliar, Stack* destino, int max_altura, Stack* origem0, Stack* auxiliar0, Stack* destino0) { 
 	//Move <qtd_discos> discos da origem até destino usando auxiliar como intermediário
 	if (qtd_discos == 1) {
@@ -64,33 +97,4 @@ void mover_discos(int qtd_discos, Stack* origem, Stack* auxiliar, Stack* destino
 		
 		mover_discos(qtd_discos - 1, auxiliar, origem, destino, max_altura, origem0, auxiliar0, destino0);
 	}
-}
-
-void resolver_torre_hanoi(int altura, Stack* origem, Stack* auxiliar, Stack* destino) { 
-	// Leva (2^qtd_discos - 1) passos para resolver
-	mover_discos(altura, origem, auxiliar, destino, altura, origem, auxiliar, destino);
-}
-
-int main() {
-	int qtd_discos;
-	
-	printf("TORRE DE HANÓI \nQuantos discos na primeira torre: ");
-	scanf("%d", &qtd_discos);
-	
-	Stack* origem = new_stack(qtd_discos);
-	Stack* auxiliar = new_stack(qtd_discos);
-	Stack* destino = new_stack(qtd_discos);
-	
-	for (int i = qtd_discos; i > 0; i--) 
-		push(origem, i);
-	
-	imprimir_torres(qtd_discos, 3, origem, auxiliar, destino);
-	
-	resolver_torre_hanoi(qtd_discos, origem, auxiliar, destino);
-	
-	destroy_stack(origem);
-	destroy_stack(auxiliar);
-	destroy_stack(destino);
-	
-	return 0;
 }
